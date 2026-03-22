7c7fd712b3bc3b6d07f7635f2114ceebdf322f28
OleksandrDarchyk <oledar01@easv365.dk>
2026-03-22 19:04:03 +0100

    cash

--- DevOpsWebApplication/Program.cs
+++ DevOpsWebApplication/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
